351d261954feba91cc711c321259d4e700a4c5d3
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-10-29 11:50:18 +0600

    create user router

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
     res.send("Node server running");
 });
 
+const users = [
+    { id: 1, name: "Avi", email: "avi@gmail.com" },
+    { id: 2, name: "Robi", email: "robi@gmail.com" },
+    { id: 3, name: "Kobi", email: "kobi@gmail.com" }
+];
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
 app.listen(port, () => {
     console.log(`Simple server running on port ${port}`);
 })
