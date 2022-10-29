ffed13ae2e865c46d8f386a5a3d922990ed91ba8
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-10-29 12:17:26 +0600

    added a middlewire cross origin resources

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send("Node server running");
 });
+
+app.use(cors());
 
 const users = [
     { id: 1, name: "Avi", email: "avi@gmail.com" },
